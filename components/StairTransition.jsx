3a5894b828116d611cf1bb0f5be9fa8a5270ef1e
Muzammil Hussain <smhz101@gmail.com>
2024-10-07 20:43:48 +0500

    transition effect added

---
+++ components/StairTransition.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+// components
+import Stairs from "./Stairs";
+
+const StairTransition = () => {
+  const pathname = usePathname();
+  return (
+    <AnimatePresence mode="wait">
+      <div key={pathname}>
+        <div className="h-screen w-screen fixed top-0 left-0 right-0 pointer-events-none z-40 flex">
+          <Stairs />
+        </div>
+
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: { duration: 0.4, delay: 1, ease: "easeInOut" },
+          }}
+          className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+        />
+      </div>
+    </AnimatePresence>
+  );
+};
+
+export default StairTransition;
